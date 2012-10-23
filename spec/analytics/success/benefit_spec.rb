03860e1c335ba51d2bb581c68b27ec111f659ac6
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-10-23 18:01:10 +0100

    Test events on benefits and guides.
    author: @jthoenes
    author: @pbadenski

--- spec/analytics/success/benefit_spec.rb
+++ spec/analytics/success/benefit_spec.rb
@@ -6,15 +6,26 @@
 
 require_relative "../../spec_helper"
 
-BENEFIT_PATH = "/legal-aid"
+BENEFIT_LEGAL_AID = {
+    :path => "/legal-aid",
+    :format => "MS_programme",
+    :need_id => "620"
+}
 
 describe "benefit success tracking" do
   before(:each) do
-    visit BENEFIT_PATH
+    GoogleAnalytics.clear
+    visit BENEFIT_LEGAL_AID[:path]
+  end
+
+  it "should track an entry event" do
+    events = GoogleAnalytics.fetch_events
+    events.should have(1).item
+    events.should include_entry_for(BENEFIT_LEGAL_AID)
   end
 
   it "should show the benefit page" do
-    current_path.should == BENEFIT_PATH
+    current_path.should == BENEFIT_LEGAL_AID[:path]
   end
 
   it "should allow clicking an internal link" do
@@ -24,6 +35,10 @@ describe "benefit success tracking" do
     # click the first again, this should not cause another success
     href = find_and_click_link("Overview")
     current_path.should == URI.parse(href).path
+
+    events = GoogleAnalytics.fetch_events
+    events.should have(2).item
+    events.should include_success_for(BENEFIT_LEGAL_AID)
   end
 
   it "should allow pressing return on an internal link" do
@@ -33,19 +48,33 @@ describe "benefit success tracking" do
     # press the first again, this should not cause another success
     href = find_and_press_link("Overview", 1)
     current_path.should == URI.parse(href).path
+
+    events = GoogleAnalytics.fetch_events
+    events.should have(2).item
+    events.should include_success_for(BENEFIT_LEGAL_AID)
   end
 
   it "should allow clicking on an external link" do
     href = find_and_click_link("Scotland")
     current_url.should == href
+
+    events = GoogleAnalytics.fetch_events
+    events.should have(1).item
   end
 
   it "should allow pressing return on an external link" do
     href = find_and_press_link("Scotland", 5)
     current_url.should == href
+
+    events = GoogleAnalytics.fetch_events
+    events.should have(1).item
   end
 
   it "should fire a success after 7 seconds" do
     sleep(7)
+
+    events = GoogleAnalytics.fetch_events
+    events.should have(2).item
+    events.should include_success_for(BENEFIT_LEGAL_AID)
   end
 end
--- spec/analytics/success/guide_spec.rb
+++ spec/analytics/success/guide_spec.rb
@@ -6,15 +6,26 @@
 
 require_relative "../../spec_helper"
 
-GUIDE_PATH = "/transport-disabled"
+TRANSPORT_DISABLED_GUIDE = {
+    :path => "/transport-disabled",
+    :format => "MS_guide",
+    :need_id => "1385"
+}
 
 describe "guide success tracking" do
   before(:each) do
-    visit GUIDE_PATH
+    GoogleAnalytics.clear
+    visit TRANSPORT_DISABLED_GUIDE[:path]
+  end
+
+  it "should track an entry event" do
+    events = GoogleAnalytics.fetch_events
+    events.should have(1).item
+    events.should include_entry_for(TRANSPORT_DISABLED_GUIDE)
   end
 
   it "should show the guide page" do
-    current_path.should == GUIDE_PATH
+    current_path.should == TRANSPORT_DISABLED_GUIDE[:path]
   end
 
   it "should allow clicking an internal link" do
@@ -24,6 +35,10 @@ describe "guide success tracking" do
     # click the first again, this should not cause another success
     href = find_and_click_link("Trains")
     current_path.should == URI.parse(href).path
+
+    events = GoogleAnalytics.fetch_events
+    events.should have(2).item
+    events.should include_success_for(TRANSPORT_DISABLED_GUIDE)
   end
 
   it "should allow pressing return on an internal link" do
@@ -33,20 +48,35 @@ describe "guide success tracking" do
     # press the first again, this should not cause another success
     href = find_and_press_link("Trains", 1)
     current_path.should == URI.parse(href).path
+
+    events = GoogleAnalytics.fetch_events
+    events.should have(2).item
+    events.should include_success_for(TRANSPORT_DISABLED_GUIDE)
   end
 
   it "should allow clicking on an external link" do
     href = find_and_click_link("National Rail")
     current_url.should == href
+
+    # Are not tracked currently
+    events = GoogleAnalytics.fetch_events
+    events.should have(1).item
   end
 
   it "should allow pressing return on an external link" do
     href = find_and_press_link("National Rail", 5)
     current_url.should == href
-  end
 
+    # Are not tracked currently
+    events = GoogleAnalytics.fetch_events
+    events.should have(1).item
+  end
 
   it "should fire a success after 7 seconds" do
     sleep(7)
+
+    events = GoogleAnalytics.fetch_events
+    events.should have(2).item
+    events.should include_success_for(TRANSPORT_DISABLED_GUIDE)
   end
 end
